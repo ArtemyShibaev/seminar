2b52bcccb2964f5be757102ff9a9b7068acece86
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-09-20 11:27:37 +0300

    added task07

---
+++ task07/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 7. Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает последнюю цифру
+этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+ */
+
+//using Internal;
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+ int lastDigit = number % 10;
+ Console.Write($"последняя цифра числа: {lastDigit}");   
+}
+
+else{Console.Write("Ошибка ввода!");
+
+}
+

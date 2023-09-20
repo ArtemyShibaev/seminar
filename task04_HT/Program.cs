230cc987a48e3b834b62eed3e7622c5509642e22
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-09-20 23:58:27 +0300

    corrected code

--- task04_HT/Program.cs
+++ task04_HT/Program.cs
@@ -13,7 +13,7 @@ int number3 = Convert.ToInt32(Console.ReadLine());
 
 int max = number1;
 
-if(number1 > max) max = number1;
+//if(number1 > max) max = number1; лишнее условие
 if(number2 > max) max = number2;
 if(number3 > max) max = number3;
 

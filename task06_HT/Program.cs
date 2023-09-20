ba026467c5f1eb4f5c8c1bd5a1e34ac4fdd6adf0
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-09-20 16:13:12 +0300

    added task06

---
+++ task06_HT/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет */
+
+//using Internal;
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber % 2 == 0)
+{
+    Console.WriteLine($"число {userNumber} четное");
+}
+else
+{
+    Console.WriteLine($"число {userNumber} не является четным");
+}

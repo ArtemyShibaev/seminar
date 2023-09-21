3491cb936382994bdf3d30fad2aa0b062a6152df
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-09-21 11:09:32 +0300

    added function in code

--- task09/Program.cs
+++ task09/Program.cs
@@ -10,11 +10,18 @@ int number = new Random().Next(10,100);
 Console.WriteLine($"Случайное число из отреза 10-99 -> {number}");
 
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
-if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа ->  {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа ->  {secondDigit}");
+// if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа ->  {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа ->  {secondDigit}");
 
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа ->  {maxDigit}");
 
-
+static int MaxDigit(int num)
+{
+        int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}

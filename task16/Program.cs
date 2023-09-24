5d018d9e7f7e5c469580193cb47d78b21c40f4f6
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-09-24 23:49:02 +0300

    added task16

---
+++ task16/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 16. Напишите программу, которая принимает на
+вход два числа и проверяет, является ли одно
+число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет  */
+
+//using System.Reflection;
+
+int GetUserInput(string str)
+{
+Console.Write($"{str}: ");
+int num = Convert.ToInt32(Console.ReadLine());
+return num;
+}
+
+
+bool IsSquareTWoNumbers(int number1, int number2)
+{
+    return number1 == number2 * number2 || number2 == number1 * number1;
+}
+
+
+int num1 = GetUserInput("Введите первое число: ");
+
+int num2 = GetUserInput("Введите второе число: ");
+
+bool result = IsSquareTWoNumbers(num1, num2);
+Console.WriteLine(result ? "да" : "нет");

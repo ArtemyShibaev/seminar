5bb8de0e0192f5da83a055576164a41b50c33d35
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-09-26 13:36:36 +0300

    edited code

--- task17/Program.cs
+++ task17/Program.cs
@@ -8,16 +8,16 @@ int Quater(int x, int y)
 {
     if (x > 0 && y > 0) return 1;
     if (x < 0 && y > 0) return 2;
-    if (x < 0 && y < 0) return 3;    
+    if (x < 0 && y < 0) return 3;
     if (x > 0 && y < 0) return 4;
     return 0;
 }
 
- Console.WriteLine("Введите координаты точки: ");
- Console.Write("X: ");
- int xCoordinate = Convert.ToInt32(Console.ReadLine());
- Console.Write("Y: ");
- int yCoordinate = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки: ");
+Console.Write("X: ");
+int xCoordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
 
 int quater = Quater(xCoordinate, yCoordinate);
 

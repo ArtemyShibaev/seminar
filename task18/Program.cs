65be59d82a0b2dc0f1d569e20128b9fc79c07ce1
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-09-26 13:37:16 +0300

    added task18

---
+++ task18/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 18: Напишите программу, которая по
+заданному номеру четверти, показывает диапазон
+возможных координат точек в этой четверти (x и y). */
+
+using System.Globalization;
+
+string RangeCoordinates(int num)
+{
+
+    if (num == 1) return "x > 0, y > 0";
+    if (num == 2) return "x < 0, y > 0";
+    if (num == 3) return "x < 0, y < 0";
+    if (num == 4) return "x > 0, y < 0";
+    return null;
+}
+
+Console.WriteLine("Введите номер четверти: ");
+
+int quater = Convert.ToInt32(Console.ReadLine());
+
+string range = RangeCoordinates(quater);
+
+Console.WriteLine(range);
+
+
+

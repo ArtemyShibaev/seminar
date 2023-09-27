45434d64771873cebf9c6a6b24e261a041af2a1a
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-09-28 01:07:14 +0300

    added task23_HT

---
+++ task23_HT/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Внутри класса Answer напишите метод ShowCube, 
+который принимает на вход число (N) 
+и выводит таблицу кубов чисел от 1 до N (включительно) 
+каждое на новой строке. */
+
+
+
+using System.Runtime.Versioning;
+
+void ShowCube(int N)
+{
+    int count = 1;
+    while (count <= N)
+    {
+        Console.WriteLine(count * count * count);
+        count++;
+    }
+}
+
+
+Console.WriteLine("Digit input from user:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+ShowCube(userNum);

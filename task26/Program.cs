959853ba655ca938d217846f1a0d37a467708dbb
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-10-02 12:00:31 +0300

    added task26

---
+++ task26/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 26: Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+ */
+
+//using Internal;
+
+int Length(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        count++;
+        num = num/10;
+    }
+
+    return count;
+}
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int length = Length(number);
+
+Console.WriteLine($"{number} -> {length}");
+

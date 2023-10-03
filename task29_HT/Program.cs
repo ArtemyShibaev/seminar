9a7dc5e872b9f4f7c9cc0ac8f83c41781ec07eec
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-10-03 22:10:06 +0300

    added task29_HT

---
+++ task29_HT/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 29: Напишите программу,
+ которая задаёт массив из 8 элементов 
+ и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+
+int[] RandomArray(int arrLength)
+{
+    int[] array = new int[arrLength];
+    Random rand = new Random();
+    for (int i = 0; i < arrLength; i++)
+    {
+        array[i] = rand.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    string output = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        output += (i < array.Length - 1) ? $"{array[i]}," : $"{array[i]}]";
+
+    }
+    Console.WriteLine(output);
+}
+
+int[] arr = RandomArray(8);
+PrintArray(arr);

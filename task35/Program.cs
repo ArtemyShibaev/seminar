bd88210c87a2fa810b685a7df9a71237b640d73a
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-10-04 15:47:05 +0300

    added task35

---
+++ task35/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Задача 35: Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в
+отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5 */
+
+
+//using Internal;
+
+int[] CreateArrayRndInt(int size, int min, int max)               // Создание одномерного массива с рандомными числами
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+
+void PrintArray(int[] arr)                                       // Вывод массива в нужном виде
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+
+int CountTwoDigitNumbers(int[] array, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i] >= min && array[i] <= max)
+            count++;
+    }
+    return count;
+}
+
+
+int[] arr = CreateArrayRndInt(123, 0, 200);
+PrintArray(arr);
+int countNumbers = CountTwoDigitNumbers(arr, 10, 99);
+Console.WriteLine($"количество двухзначных чисел   = {countNumbers}"); 

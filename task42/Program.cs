afbd40d911044fab18005ccaeac0f8fcb249bc58
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-10-06 16:46:37 +0300

    added task42

---
+++ task42/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 42: 
+Напишите программу, которая будет преобразовывать
+десятичное число в двоичное.
+46 -> 101110
+13 -> 1101
+2 -> 10 */
+
+
+
+int GetUserInput(string str)
+{
+    Console.Write($"{str}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+string DecimalToBinary(int number)
+{
+    string result = String.Empty;
+    while (number != 0)
+    {
+        result = number % 2 + result;
+        number /= 2;
+    }
+    return result;
+}
+
+
+int userNumber = GetUserInput("Введите число: ");
+string res = DecimalToBinary(userNumber);
+Console.WriteLine($"{userNumber} -> {res}");

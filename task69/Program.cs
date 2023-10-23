b9187081f1cd059a22f370203c1eb2c006652a27
Artemy Shibaev <artemy.shibaev@gmail.com>
2023-10-23 13:13:16 +0300

    added task 69

---
+++ task69/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 69: 
+Напишите программу, которая на вход принимает два числа A и B, и
+возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 */
+
+Console.WriteLine("Введите натуральное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+int RaisingAtoPowerB(int a, int b)
+{
+    if (b == 0) return 1;
+    return a * RaisingAtoPowerB(a, b - 1);
+}
+
+
+Console.WriteLine($" A = {numberA}; B = {numberB} -> {RaisingAtoPowerB(numberA, numberB)}");
